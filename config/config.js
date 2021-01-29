1d38909a28afeeeeeb1213e926d3a18123e84175
melinanisenbaum <melinanisenbaum@gmail.com>
2021-01-29 12:11:07 -0300

    01-29-2021.3

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || '127.0.0.1',
-    PORT: process.env.PORT || '3000'
+    PORT: process.env.PORT || '3300'
 }
